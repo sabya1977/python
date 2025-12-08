33f0816f2e9d1438be8b9f425a1022c49c045f95
sabyasachi <sabyasachihyd2003@gmail.com>
2025-12-08 23:24:28 +0530

    automate-boring-stuff-with-python-chatp08

---
+++ Automate-Boring-Stuff/practice/source/chapter08_strings/stringsDemp.py
@@ -0,0 +1,11 @@
+# escape characters
+print ('say Hi to Bob\'s mother')
+print ('Kashmit is called \"Paradise on Earth"')
+print ("Hi Nancy, \n\nHow are you?\nI am doing fine\nThanks,\nSabya")
+# precede backslah with a backslah to print it as backslah since blackslash is a escapre seq.
+print ("File Path: C:\\Users\\sabya\\Appdata")
+#
+# raw strings
+# raw strings ignores all escape characters
+print (r'File Path (raw strings): C:\Users\sabya\Appdata')
+# multi
